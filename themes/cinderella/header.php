48c5bfb763008cf64bf73bd7b300668a16b122d8
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2016-11-29 11:16:24 -0800

    added nav to header

--- themes/cinderella/header.php
+++ themes/cinderella/header.php
@@ -26,7 +26,6 @@
 								<li>link 1</li>
 								<li>link 2</li>
 								<li>link 3</li>
-								<li>link 4</li>
 							</ul>
 							<i class="nav-close fa fa-times-circle-o"></i> 
 						</div>
