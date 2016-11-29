a333e25196c0e69463e9e879998e9a5ffa042d6a
Sharifa <shar.samuels@gmail.com>
2016-11-29 09:59:04 -0800

    added page templates

---
+++ themes/cinderella/boutique-day.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying the Boutique Day page.
+ *
+ * @package Cinderella_Project_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+<!-- add custom fields-->
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
