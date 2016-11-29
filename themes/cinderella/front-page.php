015fcddf178912c278968c8567db991241a5e528
Sharifa <shar.samuels@gmail.com>
2016-11-29 12:56:34 -0800

    added content to home page

--- themes/cinderella/front-page.php
+++ themes/cinderella/front-page.php
@@ -10,19 +10,41 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		 <section class = "home_photo">
-			<h2><?php echo get_the_title();  ?></h2>
+		 <section class = "home_section_1">
+		 <div class = "home_photo">
+			<h2><?php echo get_the_title();?></h2>
+			<img src="<?php echo get_template_directory_uri() ?>/images/test.svg" />
+		</div>
         </section>
-		<section class="home_copy">
+		<section class="home_section_2">
  			
 			<?php echo CFS()->get( 'section_2_title' ); ?>
 			<?php echo CFS()->get( 'home_section_2_copy' ); ?>
             <?php echo CFS()->get( 'home_section_2_link_1' ); ?>
             <?php echo CFS()->get( 'home_section_2_link_2' ); ?>
 		</section>
-
-          
-
+		<section class="home_section_3">
+			<?php echo CFS()->get( 'home_quote' ); ?>
+			<?php echo CFS()->get( 'home_quote_author' ); ?>
+		</section>
+		<section class="home_section_4">
+ 			<!-- add student section if confirmed-->
+		</section>
+		<section class="home_section_5">
+ 			
+			<?php 
+			$fields = CFS()->get( 'home_stats' );
+			foreach ( $fields as $field ) {
+   			 	echo $field['image_stat'];
+    			echo $field['title_stat'];
+				echo $field['description_stat'];}
+			?>
+		</section>
+		<section class = "home_section 6">
+			<h2>No gift is too small</h2>
+			<p>	Help us contribute to these student’s stories.</p>
+			<p><a class="donate" href="<?php the_permalink(); ?>">Ways to Help</a></p>
+		</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php get_footer(); ?>
