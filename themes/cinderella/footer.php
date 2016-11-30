9c96eab4821c035d27a560817d946f943d69ecc6
ermiljordan <ermiljordan64@gmail.com>
2016-11-30 15:24:20 -0800

    fix footer fix mixin

--- themes/cinderella/footer.php
+++ themes/cinderella/footer.php
@@ -8,39 +8,36 @@
 ?>
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
+				<div class="site-info container">
 					<div class="globe-logo">
 						<img src = "<?php echo get_template_directory_uri(); ?>/images/globe-icon.svg" alt = "Globe Icon">
 					</div>
 					
 					<div class="navigation">
-						<div class="first-nav">
-							<a href="#">Home</a>
-							<a href="#">About</a>
-							<a href="#">Boutique Day</a>
-							<a href="#">Get Involed</a>
-						</div>
-						<div class="second-nav">
-							<a href="#">Refer a Student</a>
-							<a href="#">Blog</a>
-							<a href="#">Contact Us</a>
-							<a href="#">Account</a>
+						<ul class="first-nav">
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About</a></li>
+							<li><a href="#">Boutique Day</a></li>
+							<li><a href="#">Get Involed</a></li>
+							<li><a href="#">Refer a Student</a></li>
+							<li><a href="#">Blog</a></li>
+							<li><a href="#">Contact Us</a></li>
+							<li><a href="#">Account</a></li>
+						</ul>
+						<div class="logoDevelopment">
+							<img src = "<?php echo get_template_directory_uri(); ?>/images/globe-icon.svg" alt = "Globe Icon">
 						</div>
 					</div>
 
-					<div class="logoDevelopment">
-						<img src = "<?php echo get_template_directory_uri(); ?>/images/globe-icon.svg" alt = "Globe Icon">
-					</div>
-
-					<div class="facebook">
+					<div class="facebook-icon">
 						<img src = "<?php echo get_template_directory_uri(); ?>/images/facebook-icon.svg" alt = "Facebook Icon">
 					</div>
 					
 					<div class="information">
-						<li>Registered Canadian Charitable Organization BN 86666 0699 RR0001</li>
-						<li>The Cinderella Project 2016</li>
-						<li>Mailing address: #172-219 Mackay Road, North Vancouver, BC V7P 3N6</li>
-					</li>
+						<p>Registered Canadian Charitable Organization BN 86666 0699 RR0001</p>
+						<p>The Cinderella Project 2016</p>
+						<p>Mailing address: #172-219 Mackay Road, North Vancouver, BC V7P 3N6</p>
+					</div>
 					</div><!-- #content -->
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
