354862ce491f7acf041e6dde2d2e827f10faea01
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2016-12-01 15:12:24 -0800

    added styling clases

--- themes/cinderella/page-templates/boutique-day.php
+++ themes/cinderella/page-templates/boutique-day.php
@@ -13,14 +13,14 @@ get_header(); ?>
 			<div id="fullpage">
 				<section class= "section bd_gallery">
 					<div class= "bd_photo">
-						<h2 class="boutique-day-message"><?php echo CFS()->get( 'boutique_day_message' ); ?></h2>
+						<h2 class="boutique-day-message"><?php echo CFS()->get( 'bd_message' ); ?></h2>
 						
 						<div class="carousel">
 							<?php
-								$fields = CFS()->get( 'boutique_day_gallery' );
+								$fields = CFS()->get( 'bd_gallery' );
 								foreach ( $fields as $field ) :
 							?>
-							<img class="carousel-image" src="<?php echo $field['boutique_day_gallery_image'];?>" />
+							<img class="carousel-image" src="<?php echo $field['bd_gallery_image'];?>" />
 							<?php endforeach ?>
 						</div>
 
@@ -32,36 +32,46 @@ get_header(); ?>
 					<section class="bd_section_2 cp-marketing-section">
 						<h2 class="cp-marketing-title"><?php echo CFS()->get( 'bd_title' ); ?></h2>
 						<span class="cp-horizontal-line"></span>
-						<p class="cp-marketing-copy"><?php echo CFS()->get( 'bd_description' ); ?></p>
+						<div class="cp-marketing-copy"><?php echo CFS()->get( 'bd_description' ); ?></div>
 					</section>
+
 					<!--You made it possible quote-->
-
-								<section class="section home-section-3 cp-quote-section">
-					<h2 class="cp-quote-wrapper"><span class="cp-quote-text"><?php echo CFS()->get( 'home_quote' ); ?></span></h2>
-					
-				</section>
-
 					<section class="bd_section_3 cp-quote-section">
 						<h2 class="cp-quote-wrapper"><span class="cp-quote-text"><?php echo CFS()->get( 'bd_quote' ); ?></span></h2>
 						<h3 class="cp-quote-author"><?php echo CFS()->get( 'bd_author' ); ?></h3>
 					</section>
 					<!--Students who attend BD are provided...-->
-					<section class="bd_section_4">
-						<?php echo CFS()->get( 'bd_about_bd' ); ?>
+					<section class="bd_section_4 cp-description-section">
+						<div class="cp-description-text"><?php echo CFS()->get( 'bd_about_paragraph' ); ?></div>
+					</section>
+
+					<section>
 						<h2>What to expect</h2>
 					</section>
+
 					<!--What to expect image and copy grid-->
 					<section class="bd_section_5">
+					<section class="cp-checkered-section">
+							<div style="display:none">
+								<div class="cp-checkered-image-wrapper"></div>
+								<div class="cp-checkered-text-wrapper"></div>
+							</div>
 						<?php 
-							$fields = CFS()->get( 'bd_what_to_expect' );
-							foreach ( $fields as $field ) :
-							?>
-							<img src = "<?php echo $field['bd_what_to_expect_image'];?>" />
-							<?php	
-								echo $field['bd_what_to_expect_copy'];
-							?>
-							<?php endforeach ?>
+						$fields = CFS()->get( 'bd_what_to_expect' );
+						foreach ( $fields as $field ) :
+						?>
+						<div class="cp-checkered-container">
+							<div class="cp-checkered-image-wrapper" style="background: url(<?php echo $field['bd_what_to_expect_image'];?>); background-size: cover;"></div>
+							<div class="cp-checkered-text-wrapper">
+								<p class="cp-checkered-description"><?php echo $field['bd_what_to_expect_copy'];?></p>
+							</div>
+						</div>
+						<?php endforeach ?>
 					</section>
+					</section>
+
+
+
 					<!--The Student resource kit-->
 					<section class="bd_section_6">
 								<?php echo CFS()->get( 'bd_resource_kit' ); ?>
@@ -82,8 +92,9 @@ get_header(); ?>
 						<p>Help us contribute to these student’s stories.</p>
 						<p><a class="register" href="<?php the_permalink(); ?>">Ways to help</a></p>
 					</section>
+					<?php get_footer(); ?>
 				</section>
 			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
-<?php get_footer(); ?>
+
