c88b64af799d49733e7926111403bcd8101fea22
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2016-12-01 10:38:44 -0800

    prefixed styles with cp for cinderella project

--- themes/cinderella/front-page.php
+++ themes/cinderella/front-page.php
@@ -16,39 +16,42 @@ get_header(); ?>
 					<h2 class="brand-title"><?php bloginfo('name');?></h2>
 					<span class="horizontal-line"></span>
 					<p class="brand-slogan">Your story begins here.</p>
-					<i class="angle-down fa fa-angle-down"></i>
+					<i class="cp-angle-down fa fa-angle-down"></i>
 				</section>
 
-				<section class="section home-section-2">
-					<h2 class="title"><?php echo CFS()->get( 'section_2_title' ); ?></h2>
-					<span class="horizontal-line"></span>
-					<p class="copy"><?php echo CFS()->get( 'home_section_2_copy' ); ?></p>
-					<a class="button-link" href="<?php the_permalink(); ?>">About Boutique Day</a>
-					<a class="learn-about-link" href="<?php the_permalink(); ?>">Learn about the Cinderella Project</a>
-					<i class="angle-down fa fa-angle-down"></i>
+				<section class="section home-section-2 cp-marketing-section">
+					<h2 class="cp-marketing-title"><?php echo CFS()->get( 'section_2_title' ); ?></h2>
+					<span class="cp-horizontal-line"></span>
+					<p class="cp-marketing-copy"><?php echo CFS()->get( 'home_section_2_copy' ); ?></p>
+					<a class="cp-button-link" href="<?php the_permalink(); ?>">About Boutique Day</a>
+					<a class="cp-learn-about-link" href="<?php the_permalink(); ?>">Learn about the Cinderella Project</a>
+					<i class="cp-angle-down fa fa-angle-down"></i>
 				</section>
 
-				<section class="section home-section-3">
-					<h2 class="quote"><span class="quote-text"><?php echo CFS()->get( 'home_quote' ); ?></span></h2>
-					<h3 class="author"><?php echo CFS()->get( 'home_quote_author' ); ?></h3>
+				<section class="section home-section-3 cp-quote-section">
+					<h2 class="cp-quote-wrapper"><span class="cp-quote-text"><?php echo CFS()->get( 'home_quote' ); ?></span></h2>
+					<h3 class="cp-quote-author"><?php echo CFS()->get( 'home_quote_author' ); ?></h3>
 				</section>
+
 				<section class="section fp-auto-height home-section-4">
-					<?php 
-					$fields = CFS()->get( 'home_stats' );
-					foreach ( $fields as $field ) :
-					?>
-					<div class="stat-container">
-						<div class="image-wrapper" style="background: url(<?php echo $field['image_stat'];?>); background-size: cover;"></div>
-						<div class="stat-text-wrapper">
-							<h3 class="stat-value"><?php echo $field['title_stat'];?></h3>
-							<p class="stat-description"><?php echo $field['description_stat'];?></p>
+					<section class="cp-checkered-section">
+						<?php 
+						$fields = CFS()->get( 'home_stats' );
+						foreach ( $fields as $field ) :
+						?>
+						<div class="cp-checkered-container">
+							<div class="cp-checkered-image-wrapper" style="background: url(<?php echo $field['image_stat'];?>); background-size: cover;"></div>
+							<div class="cp-checkered-text-wrapper">
+								<h3 class="cp-checkered-value"><?php echo $field['title_stat'];?></h3>
+								<p class="cp-checkered-description"><?php echo $field['description_stat'];?></p>
+							</div>
 						</div>
-					</div>
-					<?php endforeach ?>
+						<?php endforeach ?>
+					</section>
 
 					<h2>No gift is too small</h2>
 					<p>	Help us contribute to these student’s stories.</p>
-					<a class="button-link" href="<?php the_permalink(); ?>">Ways to Help</a>
+					<a class="cp-button-link" href="<?php the_permalink(); ?>">Ways to Help</a>
 					<?php get_footer(); ?>
 				</section>
 			</div>
