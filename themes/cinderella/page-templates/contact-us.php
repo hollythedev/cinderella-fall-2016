708cda9979c25f6865fc482941dfe759e20e371f
ermiljordan <ermiljordan64@gmail.com>
2016-11-30 21:41:45 -0800

    add contact form 7 on contact-us

--- themes/cinderella/page-templates/contact-us.php
+++ themes/cinderella/page-templates/contact-us.php
@@ -10,6 +10,30 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+			<section id="fullpage">
+				<div class="background-image">
+					<img class="band-logo">
+					<h2 class="brand-title"></h2>
+					<i class="angle-down fa fa-angle-down"></i>
+			</select>
+			<section class="registration">
+				<form>
+					<select class="drop-down">
+						<option value="Select Value">Select Value</option>
+						<option value="Student Referral">Student Referral</option>
+						<option value="Donation">Donation</option>
+						<option value="Sponsorship">Sponsorship</option>
+						<option value="Fundraising">Fundraising</option>
+						<option value="Volunteer">Volunteer</option>
+						<option value="Account">Account</option>
+						<option value="General">General</option>
+						<option value="Resource Kit">Resource Kit</option>
+						<option value="Value">Value</option>
+					<select>	
+					<!--   CONTACT FORM 7 HERE-->
+					<a class="button-link" href="http://localhost:3000/cinderella/">Send a Message</a>
+				</form>
+			</section>	
 <!-- add custom fields-->
 
 		</main><!-- #main -->
