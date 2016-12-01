f532c03ab5ddd8bd1b95f61f604736eed93e2b51
ermiljordan <ermiljordan64@gmail.com>
2016-11-30 22:33:20 -0800

    mark up on refer-a-student

--- themes/cinderella/page-templates/refer-a-student.php
+++ themes/cinderella/page-templates/refer-a-student.php
@@ -10,6 +10,62 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+			<div id="fullpage">
+				<section class="section home-section-1">
+					<img class="brand-logo"/>
+					<h2 class="brand-title"></h2>
+					<span class="horizontal-line"></span>
+					<i class="angle-down fa fa-angle-down"></i>
+				</section>
+
+				<section class="referral">
+					<div=""></div>
+				</section>
+
+				<section class="">
+					<p>
+						We accept student referrals froms school counsellors, administrators,
+						teachers, youth workers, and social workers from the following school district:
+					<p>
+						<span class="horizontal-line"></span>
+				</section>
+
+					<ul class="cities">
+						<li>Burnaby</li>
+						<li>Delta</li>
+						<li>North Vancouver</li>
+						<li>Richmond</li>
+						<li>Squamish, Whistler, Pemberton</li>
+						<li>Sunshine Coast
+							<span>(Gibson, Pender Harbour, Sechelt)</span>
+						</li>
+						<li>Vancouver</li>
+						<li>West Vancouver</li>
+					</ul>
+				
+				<section="projects">
+					<div></div>
+				</section>
+
+				<section="city-district">
+
+					<div>
+					<h2>Don't see your District?</h2>
+						<p>
+							There may be a similar
+							<span>organization for you</span>
+						</p>
+					</div>
+					
+					<form>
+						<select class="drop-down">
+							<option value="Province">Province</option>
+						</select>
+						<select class="drop-down">
+							<option class="Penticton">Penticton</option>
+						</select>
+			</div>
+
 <!-- add custom fields-->
 
 		</main><!-- #main -->
