9211ea46b27d3a663c537312c820144f3244f76b
Scott Fuoco <scottfuoco@Scotts-MBP.hitronhub.home>
2016-12-03 15:55:33 -0800

    changed accordion from ID to class for inner accordions

--- themes/cinderella/js/app.js
+++ themes/cinderella/js/app.js
@@ -2,7 +2,7 @@
 
     $(document).ready(function() {
 
-        $( "#accordion" ).accordion({
+        $( ".accordion" ).accordion({
             collapsible: true,
             active: false,
             heightStyle: 'content'
