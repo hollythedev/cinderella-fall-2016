704006c965b6144af1cfc3d3a38041d44f51c6b7
Scott Fuoco <scottfuoco@Scotts-MBP.hitronhub.home>
2016-12-03 12:57:38 -0800

    initial commit

---
+++ themes/cinderella/page-templates/registration.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Registration Page
+ *
+ * @package Cinderella_Project_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+<!-- add custom fields-->
+	<div id="fullpage">
+		<section class="section cp-landing-section">
+			Landing Page
+		</section>
+
+		<section class="section fp-auto-height cp-registration-section">
+			<?php echo CFS()->get( 'registration_form' ); ?>
+		</section>
+	</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
