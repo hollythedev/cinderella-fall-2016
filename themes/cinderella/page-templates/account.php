9a34143c256e0d21f8023378f9c62c66eeb05264
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2016-12-02 16:03:34 -0800

    display login form

--- themes/cinderella/page-templates/account.php
+++ themes/cinderella/page-templates/account.php
@@ -9,6 +9,13 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+
+						<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+			
 			<div id="fullpage">
 				<section class="">
 					<div class="account-photo">
@@ -30,5 +37,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
