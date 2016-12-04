a6add33dd557125d082595b86b07fa83a1282f69
ermiljordan <ermiljordan64@gmail.com>
2016-12-03 17:42:43 -0800

    fixed classed on contact-us

--- themes/cinderella/page-templates/contact-us.php
+++ themes/cinderella/page-templates/contact-us.php
@@ -9,24 +9,20 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		
-		<div class="contact-us-page">
-			<section class="section contact-us-1">
-				<div class="contact-us-background">
+			<div id="fullpage">
+				<section class="section contact-us-section-1">
 					<h2 class="contact-us-title"><?php echo CFS()->get( 'contact_us_message' ); ?></h2>
 						<span class='horizontal-line'></span>
-							<i class="angle-down fa fa-angle-down fa-3x"></i>
-				</div>
-			</select>
-			
-			<section class="registration">
-				<div class="contactform">
-					<?php echo CFS()->get( 'contact_us_select_menu' ); ?>
-				<form>
-					<a class="button-link" href="http://localhost:3000/cinderella/">Send a Message</a>
-				</div>
-			</section>
-		</div>	
+						<i class="angle-down fa fa-angle-down fa-3x"></i>
+				</select>
+
+				<section class="section fp-auto-height">
+					<section class="section contact-us-section-2">
+						<h2 class="cp-contactform"><?php echo CFS()->get( 'contact_us_select_menu' ); ?><h2>
+						<a class="button-link" href="http://localhost:3000/cinderella/">Send a Message</a>
+					</section>
+				</section><!-- end of fp-auto-height -->
+		</div><!-- #end of fullpage -->	
 <!-- add custom fields-->
 
 		</main><!-- #main -->
