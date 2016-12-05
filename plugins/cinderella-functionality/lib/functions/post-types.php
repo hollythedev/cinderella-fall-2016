f21437ce2399fa8d11bb9569db1ca5fce4f82d9e
Scott Fuoco <scottfuoco@Scotts-MBP.hitronhub.home>
2016-12-05 12:50:45 -0800

    added resource custom post type

--- plugins/cinderella-functionality/lib/functions/post-types.php
+++ plugins/cinderella-functionality/lib/functions/post-types.php
@@ -5,9 +5,10 @@
  * @link  http://codex.wordpress.org/Function_Reference/register_post_type
  */
 
-// Register Wishlist Post Type
-function register_wishlist_post_type() {
 
+function register_custom_post_types() {
+
+// Register Wishlist Post Type
 	$labels = array(
 		'name'                  => 'Wishlists',
 		'singular_name'         => 'Wishlist',
@@ -56,5 +57,54 @@ function register_wishlist_post_type() {
 	);
 	register_post_type( 'wishlist', $args );
 
+// Register Resources Post Type
+	$labels = array(
+		'name'                  => 'Resources',
+		'singular_name'         => 'Resource',
+		'menu_name'             => 'Resources',
+		'name_admin_bar'        => 'Resource',
+		'archives'              => 'Resource',
+		'parent_item_colon'     => 'Parent Resource:',
+		'all_items'             => 'All Resource',
+		'add_new_item'          => 'Add New Resource',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New Resource',
+		'edit_item'             => 'Edit Resource',
+		'update_item'           => 'Update Resource',
+		'view_item'             => 'View Resource',
+		'search_items'          => 'Search Resource',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into wishlist',
+		'uploaded_to_this_item' => 'Uploaded to this wishlist',
+		'items_list'            => 'Resources list',
+		'items_list_navigation' => 'Resources list navigation',
+		'filter_items_list'     => 'Filter wishlists list',
+	);
+	$args = array(
+		'label'                 => 'Resource',
+		'description'           => 'Resources items for the Cinderella Project',
+		'labels'                => $labels,
+		'supports'              => array( 'title'),
+		'hierarchical'          => false,
+		'public'                => false,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 10,
+		'menu_icon'             => 'dashicons-list-view',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => false,
+		'has_archive'           => 'resource',		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+		'show_in_rest'       => true,
+	);
+	register_post_type( 'resource', $args );
 }
-add_action( 'init', 'register_wishlist_post_type', 0 );
+add_action( 'init', 'register_custom_post_types', 0 );
