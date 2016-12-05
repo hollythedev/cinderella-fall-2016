7ff3c2de4d89c1fddd85a9fef7d7aee2560cffe0
ermiljordan <ermiljordan64@gmail.com>
2016-12-04 16:49:51 -0800

    styling contact us

--- themes/cinderella/page-templates/contact-us.php
+++ themes/cinderella/page-templates/contact-us.php
@@ -11,14 +11,14 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<div id="fullpage">
 				<section class="section contact-us-section-1">
-					<h2 class="contact-us-title"><?php echo CFS()->get( 'contact_us_message' ); ?></h2>
+					<h2 class="cp-marketing-title"><?php echo CFS()->get( 'contact_us_message' ); ?></h2>
 						<span class="cp-horizontal-line"></span>
 						<i class="angle-down fa fa-angle-down fa-3x"></i>
 				</section>
 
 				<section class="section fp-auto-height">
 					<section class="section contact-us-section-2">
-						<h2 class="cp-contactform"><?php echo CFS()->get( 'contact_us_select_menu' ); ?><h2>
+						<p class="cp-contactform"><?php echo CFS()->get( 'contact_us_select_menu' ); ?><p>
 						<a class="button-link" href="http://localhost:3000/cinderella/">Send a Message</a>
 					</section>
 					<?php get_footer(); ?>
