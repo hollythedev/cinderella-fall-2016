502c2895897585a154ae037a52ec4ad6dba62a0f
ermiljordan <ermiljordan64@gmail.com>
2016-12-06 13:18:24 -0800

    added new CFS for contact us

--- themes/cinderella/page-templates/contact-us.php
+++ themes/cinderella/page-templates/contact-us.php
@@ -19,6 +19,10 @@ get_header(); ?>
 				<section class="section fp-auto-height">
 					<section class="section contact-us-section-2">
 						<p class="cp-contactform"><?php echo CFS()->get( 'contact_us_select_menu' ); ?><p>
+							<h2 class="cp-title">The Cinderella Project</h2>
+							<div class="cp-address"><?php echo CFS()->get(''); ?></div>
+							<div class="cp-phone-number"><?php echo CFS()->get('') ?></div>
+							<div class="cp-email"><?php echo CFS()->get(''); ?></div>
 					</section>
 					<?php get_footer(); ?>
 				</section><!-- end of fp-auto-height -->
