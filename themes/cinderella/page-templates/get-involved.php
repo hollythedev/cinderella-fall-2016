02d05668eaf2879fc48ac7832f570ee9b171894b
Sharifa <shar.samuels@gmail.com>
2016-12-08 12:21:14 -0800

    styled support us section on get involved

--- themes/cinderella/page-templates/get-involved.php
+++ themes/cinderella/page-templates/get-involved.php
@@ -63,8 +63,9 @@ get_header(); ?>
 									<label for="amount">$</label>
 									<input id="amount" type="text" placeholder="enter amount">
 								</form>
-								<a class="cp-button-link" href="<?php the_permalink(); ?>">Support Us</a>
-								<p>or</p>
+								<a class="cp-button-link" href="<?php the_permalink(); ?>">Support Us <i class="fa fa-caret-right" aria-hidden="true"></i></a>
+								<p class="horiz-line">or</p>
+								<span class="cp-horizontal-line"></span>
 								<p>Choose specific donation:</p>								
 								
 								<div class="cp-donation-grid-wrapper">
