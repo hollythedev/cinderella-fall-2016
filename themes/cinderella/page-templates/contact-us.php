68437d6a2530f5eb2ad76535ce42677109eae13c
ermiljordan <ermiljordan64@gmail.com>
2016-12-09 10:31:43 -0800

    styling

--- themes/cinderella/page-templates/contact-us.php
+++ themes/cinderella/page-templates/contact-us.php
@@ -12,26 +12,24 @@ get_header(); ?>
 			<div id="fullpage">
 				<section class="section">
 					<section class="contact-us-section-1 cp-section cp-landing-section">
-						<h2 class="cp-marketing-title"><?php echo CFS()->get( 'contact_us_message' ); ?></h2>
+						<h2 class="cp-landing-title"><?php echo CFS()->get( 'contact_us_message' ); ?></h2>
 						<span class="cp-horizontal-line"></span>
 						<i class="cp-angle-down fa fa-angle-down fa-3x"></i>
 					</section>
 				</section>
 
 				<section class="section fp-auto-height">
-					<section class="section contact-us-section-2 section-above-footer">
-						<p class="cp-contactform"><?php echo CFS()->get( 'contact_us_select_menu' ); ?><p>
-							<div class="cp-contact-info">
+					<section class="cp-purple-section section-above-footer">
+						<div class="cp-contactform"><?php echo CFS()->get( 'contact_us_select_menu' ); ?><div>
 								<h2 class="cp-title">The Cinderella Project</h2>
-								<div class="cp-contacts">
-									<?php echo CFS()->get('contact_address'); ?>
-									<?php echo CFS()->get('contact_number') ?>
-									<?php echo CFS()->get('contact_email'); ?>
-								</div>
-							</div>
+									<div class="info">
+										<?php echo CFS()->get('contact_address'); ?>
+										<?php echo CFS()->get('contact_number') ?>
+										<?php echo CFS()->get('contact_email'); ?>
+									</div>
 					</section>
 					<?php get_footer(); ?>
-				</section><!-- end of fp-auto-height -->
+				</section>  
 			</div><!-- #end of fullpage -->	
 		</main><!-- #main -->
 	</div><!-- #primary -->
