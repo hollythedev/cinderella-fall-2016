0f595c4ee25c2acc609026448e5c9e0cfc652237
Sharifa <shar.samuels@gmail.com>
2016-12-09 11:02:38 -0800

    created mixins for title section for refer student

--- themes/cinderella/page-templates/refer-a-student.php
+++ themes/cinderella/page-templates/refer-a-student.php
@@ -12,9 +12,8 @@ get_header(); ?>
 			<div id="fullpage">
 				<section class="section">
 					<section class=" referral-section-1 cp-section cp-landing-section">
-						
 						<div class="cp-landing-top-divider">
-							<h2 class="cp-marketing-title"><?php echo CFS()->get( 'brand_title' ); ?></h2>
+							<h2 class="cp-landing-title"><?php echo CFS()->get( 'brand_title' ); ?></h2>
 							<div class="cp-horizontal-line"></div>
 						</div>
 						<i class="cp-angle-down fa fa-angle-down"></i>
@@ -22,16 +21,16 @@ get_header(); ?>
 				</section>
 
 				<section class="section fp-auto-height">
-					<section class="cp-marketing-section referral-section-2">
-						<h2 class="cp-marketing-title"><?php echo CFS()->get( 'referral_message' ); ?></h2>
+					<section class="cp-title-section referral-section-2">
+						<h2 class="cp-title-refer-student-text"><?php echo CFS()->get( 'referral_message' ); ?></h2>
 					</section>
 
-					<section class="section referral-section-3">
+					<section class="referral-section-3">
 						<div class="cp-referral-student-message">
 						<?php echo CFS()->get( 'ref_a_student_message' ); ?>
 						</div>
-							<span class="horizontal-line"></span>
-						<div clss="city-district">
+							<div class="cp-horizontal-line"></div>
+						<div class="city-district">
 							<?php echo CFS()->get( 'city_district' ); ?>
 							<span class="bracket"><?php echo CFS()->get( 'cities_in_bracket' ); ?></span>
 							<?php echo CFS()->get( 'city_district_2' ); ?>
@@ -39,17 +38,17 @@ get_header(); ?>
 						<div class="student-download-form">
 							<?php echo CFS()->get( 'ref_student_download' ); ?>
 						</div>
-							<a class="cp-button-link button-color" href="http://localhost:3000/cinderella">Download Referral Form<i class="fa fa-caret-right" aria-hidden="true"></i></a>
+							<a class="cp-button-link button-color" href="#">Download Referral Form<i class="fa fa-caret-right" aria-hidden="true"></i></a>
 					</section>
 
-					<section class="cp-marketing-section referral-section-4">
-						<h2 class="cp-marketing-title"><?php echo CFS()->get( 'similar_project' ); ?></h2>
+					<section class="cp-title-orange-section referral-section-4">
+						<h2 class="cp-title-text"><?php echo CFS()->get( 'similar_project' ); ?></h2>
 					</section>
 
 					<section class=" referral-section-5 section-above-footer">
 						<h2 class="cp-district-dropdown"><?php echo CFS()->get( 'dont_see_your_district' ); ?></h2>
 						<span class="cp-organizations"><?php echo CFS()->get( 'organizations' ); ?></span>
-						<a class="cp-button-link" href="http://localhost:3000/cinderella">Find Results</a>
+						<a class="cp-button-link" href="#">Find Results</a>
 					</section>
 					<?php get_footer(); ?>
 				</section><!-- end of fp-auto-height -->	
