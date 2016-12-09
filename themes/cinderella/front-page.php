b2a7b6b5dfa4edbad299290c89c30d7311d97837
Scott Fuoco <scottfuoco@Scotts-MBP.hitronhub.home>
2016-12-08 22:14:50 -0800

    fixed html vaidation errors

--- themes/cinderella/front-page.php
+++ themes/cinderella/front-page.php
@@ -14,7 +14,7 @@ get_header(); ?>
 				<section class="section">
 					<section class="home-section-1 cp-section cp-landing-section">
 						<div class="cp-landing-top-divider">
-							<img class="brand-logo" src="<?php echo get_template_directory_uri() ?>/images/assets/cp_home-logo.svg" />
+							<img class="brand-logo" alt="Cinderella Project Logo" src="<?php echo get_template_directory_uri() ?>/images/assets/cp_home-logo.svg" />
 							<div class="cp-horizontal-line"></div>
 						</div>
 					<p class="brand-slogan">Your story begins here.</p>
@@ -25,11 +25,11 @@ get_header(); ?>
 
 				<section class="section">
 					<section class="home-section-2 cp-section cp-marketing-section">
-					<h2 class="cp-marketing-title front-page"><?php echo CFS()->get( 'section_2_title' ); ?></h2>
-					<span class="cp-horizontal-line"></span>
-					<p class="cp-marketing-copy"><?php echo CFS()->get( 'home_section_2_copy' ); ?></p>
-					<a class="cp-button-link" href="/index.php?p=26">About Boutique Day</a>
-					<p class="cp-learn-about-link"><a  href="/index.php?p=35">Learn about the Cinderella Project<i class="fa fa-caret-right" aria-hidden="true"></i></a></p>
+						<h2 class="cp-marketing-title front-page"><?php echo CFS()->get( 'section_2_title' ); ?></h2>
+						<div class="cp-horizontal-line"></div>
+						<p class="cp-marketing-copy"><?php echo CFS()->get( 'home_section_2_copy' ); ?></p>
+						<a class="cp-button-link" href="/index.php?p=26">About Boutique Day</a>
+						<p class="cp-learn-about-link"><a  href="/index.php?p=35">Learn about the Cinderella Project<i class="fa fa-caret-right" aria-hidden="true"></i></a></p>
 					</section>
 				</section>
 
@@ -57,24 +57,21 @@ get_header(); ?>
 								<div class="cp-expect-image-wrapper" style="background: url(<?php echo $field['image_what_to_expect'];?>); background-size: cover;"></div>
 							</div>
 							<?php endforeach ?>
-						</div>
+							</div>
+						</section>
 
 
-						<div class=" home-section-5 cp-no-gift">
+						<section class=" home-section-5 cp-no-gift">
 							<h2>No gift is too small</h2>
 							<p>	Help us contribute to these student’s stories.</p>
 							<a class="cp-button-link" href="/index.php?p=37">Ways to Help <i class="fa fa-caret-right" aria-hidden="true"></i></a>
-						</div>
+						</section>
 
-						<div class="home-section-6 cp-quote-section cp-section section-above-footer">
+						<section class="home-section-6 cp-quote-section cp-section section-above-footer">
 							<h2 class="cp-quote-wrapper"><span class="cp-quote-text"><?php echo CFS()->get( 'home_quote_2' ); ?></span></h2>
 							<p class="cp-quote-author"><?php echo CFS()->get( 'home_quote_author_2' ); ?></p>
-						</div>
+						</section>
 
 						<?php get_footer(); ?>
-					</section>
-				</section> <!-- end of section fp-auto-height -->
-			</div> <!-- #fullpage -->
-		</main><!-- #main -->
-	</div><!-- #primary -->
+
 
