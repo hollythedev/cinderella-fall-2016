faa78c2f71cb5e224030288b8d6c8eedab646dac
Sharifa <shar.samuels@gmail.com>
2016-12-09 08:45:46 -0800

    changed tags on about

--- themes/cinderella/page-templates/about.php
+++ themes/cinderella/page-templates/about.php
@@ -66,7 +66,7 @@ get_header(); ?>
 								<p class="cp-stat-description"><?php echo $field['about_us_referral_details'];?></p>
 							</div>
 							
-							<div class="cp-stat-image-wrapper" style="background: url(<?php echo $field['about_us_referral_image'];?>); background-size: cover;"></div>
+							<div class="cp-stat-image-wrapper grayscale-image" style="background: url(<?php echo $field['about_us_referral_image'];?>); background-size: cover;"></div>
 							</div>
 								<?php endforeach ?>
 						
