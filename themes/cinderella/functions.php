5061f323d8f27c870752317661cb9a86b1bec4cd
Scott Fuoco <scottfuoco@Scotts-MacBook-Pro.local>
2016-12-10 14:24:27 -0800

    removed margin-top added by wpadminbar

--- themes/cinderella/functions.php
+++ themes/cinderella/functions.php
@@ -102,6 +102,13 @@ function cp_starter_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'cp_starter_scripts' );
 
+
+  add_action('get_header', 'my_filter_head');
+
+  function my_filter_head() {
+    remove_action('wp_head', '_admin_bar_bump_cb');
+  }
+  
 /**
  * Custom template tags for this theme.
  */
