4441214b6160ae486fb140f8f667e8ed9c330b8a
ermiljordan <ermiljordan64@gmail.com>
2016-12-11 12:21:58 -0800

    styling

--- themes/cinderella/404.php
+++ themes/cinderella/404.php
@@ -16,8 +16,11 @@ get_header(); ?>
 				<header class="page-header">
 					<img src="<?php echo get_template_directory_uri() . '/images/assets/cp_404-fairyDust.svg'; ?>" alt="Fairy Dust Logo" />
 				</header><!-- .page-header -->
+				<section class="cp-errors">
 					<h2 class="cp-404-title"><?php echo CFS()->get( '404_message' ); ?></h2>
-					
+					<p class="cp-error-message">Error code: 404</p>
+				</section>
+				<a class="cp-button-link button-color" href="<?php the_permalink(7); ?>">Get Back Home"<i class="fa fa-caret-right" aria-hidden="true"></i></a>
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
