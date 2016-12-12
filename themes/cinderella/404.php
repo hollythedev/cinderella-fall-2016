4cb28e921d98f8f61efe82e264aabaf5eaaa38d9
ermiljordan <ermiljordan64@gmail.com>
2016-12-11 23:16:04 -0800

    fix background image

--- themes/cinderella/404.php
+++ themes/cinderella/404.php
@@ -14,11 +14,11 @@ get_header(); ?>
 
 			<section class="error-404 not-found cp-landing-section error-image">
 				<header class="page-header">
-					<img src="<?php echo get_template_directory_uri() . '/images/assets/cp_404-fairyDust.svg'; ?>" alt="Fairy Dust Logo" />
+					<img  class="not-found-wand" src="<?php echo get_template_directory_uri() . '/images/assets/cp_404-fairyDust.svg'; ?>" alt="Fairy Dust Logo" />
 				</header><!-- .page-header -->
 				<section class="cp-errors">
-          <h2><?php esc_html_e('Sorry! We must’ve run out of fairy dust for that link', 'Divi'); ?></h2>
-					<p><?php esc_html_e('Error Code: 404', 'Divi'); ?></p>
+          <h2 class="error-message"><?php esc_html_e('Sorry! We must’ve run out of fairy dust for that link', 'Divi'); ?></h2>
+					<p class"message-404"><?php esc_html_e('Error Code: 404', 'Divi'); ?></p>
 				</section>
 				<a class="cp-button-link button-color" href="<?php the_permalink(7); ?>">Get Back Home"<i class="fa fa-caret-right" aria-hidden="true"></i></a>
 			</section><!-- .error-404 -->
