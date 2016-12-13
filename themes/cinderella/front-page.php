4338fba1c98368cd3ef3581b2129e27c1ccc21e2
Sharifa <shar.samuels@gmail.com>
2016-12-13 14:11:38 -0800

    added class to learn about link on home

--- themes/cinderella/front-page.php
+++ themes/cinderella/front-page.php
@@ -29,7 +29,7 @@ get_header(); ?>
 						<div class="cp-horizontal-line"></div>
 						<p class="cp-marketing-copy"><?php echo CFS()->get( 'home_section_2_copy' ); ?></p>
 						<div class="desktop-cta">
-							<p class="cp-learn-about-link " ><a href="<?php the_permalink(35); ?>">About the Cinderella Project<i class="fa fa-caret-right" aria-hidden="true"></i></a></p>
+							<p class="cp-learn-about-link cp-learn-about-link-home" ><a href="<?php the_permalink(35); ?>">About the Cinderella Project<i class="fa fa-caret-right" aria-hidden="true"></i></a></p>
 							<a class="cp-button-link" href="<?php the_permalink(26); ?>">About Boutique Day</a>
 						</div> <!--CTA buttons for desktop -->
 					</section>
